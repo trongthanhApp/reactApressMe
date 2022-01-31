5d6d8c7fba1a30e699f2848821f618318a832200
thanh <trongthanh.nguyen.info@gmail.com>
2022-01-31 19:56:39 +0100

    componentDidMount 3

--- 13 - Reconciliation and Lifecycles/lifecycle/src/Message.js
+++ 13 - Reconciliation and Lifecycles/lifecycle/src/Message.js
@@ -35,7 +35,7 @@ export class Message extends Component {
     }
 
     componentDidMount() {
-        console.log("componentDidMount Message Component");
+        console.log("componentDidMount Message Component 3");
     }
     
     // shouldComponentUpdate(newProps, newState) {
