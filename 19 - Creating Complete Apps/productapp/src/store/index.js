36e443fbce6286a77eeddf466c40450e4abb8582
thanh <trongthanh.nguyen.info@gmail.com>
2022-02-19 23:07:01 +0100

    connect datastore products to component using redux

--- 19 - Creating Complete Apps/productapp/src/store/index.js
+++ 19 - Creating Complete Apps/productapp/src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore } from "redux";
 import modelReducer from "./modelReducer";
+
 export default createStore(modelReducer);
 export { saveProduct, saveSupplier, deleteProduct, deleteSupplier }
     from "./modelActionCreators";
